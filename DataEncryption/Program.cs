12f38f269e650822b9f9f7ea9caf73ad82b0f6ee
kishoretvk <kishoretvk@hotmail.com>
2018-07-04 23:40:37 -0500

    added a test method in program.cs to check if the key generator is working

--- DataEncryption/Program.cs
+++ DataEncryption/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataEncryption.KeyGenerator;
+using System;
 
 namespace DataEncryption
 {
@@ -7,6 +8,22 @@ namespace DataEncryption
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Lets Generate Key!, Enter your keylengith below");
+           TestKeyGen();
+            Console.ReadKey();
+        }
+        private static void TestKeyGen()
+        {
+            if (!Console.NumberLock)
+            {
+                Console.WriteLine("Numb lock is off!");
+
+            }
+            var inputKey = Console.ReadLine();
+            var keylengith = Convert.ToInt32(inputKey);
+            var keyGen = new CryptographicKeyGenerator();
+            keyGen.KeyLengith = keylengith;
+            Console.WriteLine("Key generated with length given" + keyGen.GetEncryptionKey(keylengith));
         }
     }
 }
